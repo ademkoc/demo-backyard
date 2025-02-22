84d40773ba98803176c729571de445abd36a04e4
Adem Koc <koc.adem.809@gmail.com>
2025-02-22 17:11:20 +0300

    refactor: use custom gracefull shutdown logic

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,16 @@
-import closeWithGrace from 'close-with-grace';
 import packageJson from '../package.json' with { type: 'json' };
 import { createApp } from './app.ts';
 
 async function startServer () {
-  const app = await createApp();
+  const abortController = new AbortController();
 
-  closeWithGrace(async function ({ signal, err, manual }) {
-    if (err) {
-      app.log.error({ err }, 'server closing with error');
-    } else {
-      app.log.info(`${signal} received, server closing`);
-    }
-    await app.close();
-  });
+  const app = await createApp();
 
   try {
     await app.listen({
       host: '0.0.0.0',
       port: 3000,
+      signal: abortController.signal,
       listenTextResolver: (address) => {
         return `${packageJson.name} listening at ${address}`;
       }
@@ -26,6 +19,14 @@ async function startServer () {
     app.log.error(err);
     process.exit(1);
   }
+
+  process.once('SIGTERM', (signal) => closeServer(signal));
+  process.once('SIGINT', (signal) => closeServer(signal));
+
+  const closeServer = (signal:NodeJS.Signals) => {
+    app.log.info(`Received signal to close: ${signal}`);
+    abortController.abort();
+  };
 }
 
 startServer();
