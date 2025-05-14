60aba524457727224dc2df37e44c807e59e4ea16
Adem Koc <koc.adem.809@gmail.com>
2025-05-14 17:22:14 +0300

    chore: update mikro-orm config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from '@mikro-orm/mysql';
 export default defineConfig({
   clientUrl: process.env.DATABASE_URL,
 
-  entities: ['dist/src/modules/**/*.entity.js'],
-  entitiesTs: ['src/modules/**/*.entity.ts'],
+  entities: ['src/modules/**/*.entity.ts'],
 
   migrations: {
     tableName: 'migrations',
@@ -13,6 +12,5 @@ export default defineConfig({
     transactional: true
   },
 
-  debug: false,
-  preferTs: process.env.MIKRO_ORM_PREFER_TS === 'true'
+  debug: false
 });
